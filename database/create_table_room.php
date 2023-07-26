bbff3e785de7ccbe04b945195a5f8e0aa3cb422f
Hendri Arifin <hendriarifin@Hendris-MacBook-Pro.local>
2023-07-26 21:55:32 +0800

    tes pull request

--- database/create_table_room.php
+++ database/create_table_room.php
@@ -2,7 +2,8 @@
     $conn->query("CREATE TABLE room (
         id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
         name_room varchar(255),
-        images varchar(255)
+        images varchar(255),
+        luas int
     )");
 
     $conn->query("INSERT INTO room (name_room) VALUES
